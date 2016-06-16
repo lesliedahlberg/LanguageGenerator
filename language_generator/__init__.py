6c0ca5c13c5c364d7cf1199f82cec21b4c807c8f
lesliedahlberg <lesliedahlberg@Leslies-MacBook-Pro.local>
2016-06-16 16:33:56 +0200

    GUI fixes

--- language_generator/__init__.py
+++ language_generator/__init__.py
@@ -1,5 +1,7 @@
-import sys
+import tkinter
 import random
+from tkinter import *
+
 
 class SymbolCollections:
     __collection_list = {}
@@ -55,36 +57,86 @@ class Sentence:
                 sentence += '. '
         return sentence.capitalize()
 
-class Document:
+
+class Paragraph:
     def __init__(self, sentence):
         self.__sentence = sentence
 
     def generate(self, n_sentences):
-        document = ""
+        paragraph = ""
         for i in range(0, n_sentences):
             n_words = random.randrange(5, 20)
-            document += self.__sentence.generate(n_words)
+            paragraph += self.__sentence.generate(n_words)
+        return paragraph
+
+
+class Document:
+    def __init__(self, paragraph):
+        self.__paragraph = paragraph
+
+    def generate(self, n_paragraphs):
+        document = ""
+        for i in range(0, n_paragraphs):
+            n_sentences = random.randrange(5, 20)
+            document += self.__paragraph.generate(n_sentences)
+            if i < n_paragraphs-1:
+                document += "\n\n"
         return document
 
-def main():
-    vowel_list = ['a', 'e', 'i', 'u', 'o', 'ä', 'å', 'ö']
-    consonant_list = ['r', 't', 'p', 's', 'd', 'h', 'j', 'k', 'l', 'v', 'n', 'm']
 
-    symbol = SymbolCollections()
-    symbol.add(vowel_list, 'V')
-    symbol.add(consonant_list, 'C')
+def generate_callback():
+    try:
+        n = int(n_paragraphs.get())
+        text.insert(tkinter.INSERT, document.generate(n))
+    except ValueError:
+        print("Not a number!")
 
-    structure = ['CVVV', 'CVV', 'CV']
-    syllable = Syllables(structure, symbol)
 
-    word = Word(syllable)
+def clear_callback():
+    text.delete("0.1", tkinter.END)
 
-    sentence = Sentence(word)
 
-    document = Document(sentence)
-    file = open("/Users/lesliedahlberg/Desktop/text.txt", "wb")
-    file.write(bytes(document.generate(200), "UTF-8"))
-    file.close()
+vowel_list = ['a', 'e', 'i', 'u', 'o', 'ä', 'å', 'ö']
+consonant_list = ['r', 't', 'p', 's', 'd', 'h', 'j', 'k', 'l', 'v', 'n', 'm']
 
-if __name__ == "__main__":
-    main()
+symbol = SymbolCollections()
+symbol.add(vowel_list, 'V')
+symbol.add(consonant_list, 'C')
+
+structure = ['CVVV', 'CVV', 'CV']
+syllable = Syllables(structure, symbol)
+
+word = Word(syllable)
+
+sentence = Sentence(word)
+paragraph = Paragraph(sentence)
+
+document = Document(paragraph)
+
+top = tkinter.Tk()
+
+m = PanedWindow(orient=VERTICAL)
+m.pack(fill=BOTH, expand=1)
+
+m2 = PanedWindow(orient=HORIZONTAL)
+m2.pack(fill=BOTH, expand=1)
+
+generate = tkinter.Button(top, text="Generate Text", command=generate_callback)
+clear = tkinter.Button(top, text="Clear Text", command=clear_callback)
+n_paragraphs_label = tkinter.Label(top, text="Number of Paragraphs")
+n_paragraphs = tkinter.Entry(top)
+text = tkinter.Text(top)
+
+m2.add(n_paragraphs_label)
+m2.add(n_paragraphs)
+m2.add(generate)
+m2.add(clear)
+
+m.add(m2)
+m.add(text)
+
+top.mainloop()
+
+
+
+
